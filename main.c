8c425ec6f3e9ec99dfe8011fc7e16ad921fa9c66
mebibytedraco <139500397+mebibytedraco@users.noreply.github.com>
2024-04-17 23:55:07 -0400

    Begin adding elements from mockup

--- main.c
+++ main.c
@@ -2,26 +2,71 @@
 #include <locale.h>
 #include <ncursesw/curses.h>
 
-int main(void) {
-	setlocale(LC_ALL, "");
-	initscr();
+#define CHANNELS_PANE_WIDTH 15
+#define USERS_PANE_WIDTH 15
+#define MSG_AREA_WIDTH (max_x - CHANNELS_PANE_WIDTH - USERS_PANE_WIDTH)
 
+#define COLPAIR_X_BUTTON 1
+#define COLPAIR_UL_SYMBOL 2
+#define COLPAIR_SERVER_INFO 3
+#define COLPAIR_MSG_BG 4
+
+int main(void) {
+	setlocale(LC_ALL, ""); // Enables UTF-8
+	initscr(); // Start ncurses
+
+	// Setup color (assumes color is supported)
 	start_color();
 	use_default_colors();
-	init_pair(1, -1, COLOR_BLUE);
+	init_pair(COLPAIR_X_BUTTON, -1, COLOR_RED); // white on red
+	// magenta on blue
+	init_pair(COLPAIR_UL_SYMBOL, COLOR_MAGENTA, COLOR_BLUE);
+	init_pair(COLPAIR_SERVER_INFO, -1, COLOR_MAGENTA);
+	init_pair(COLPAIR_MSG_BG, COLOR_WHITE, -1);
 
-	curs_set(0);
+	curs_set(0); // Hide the cursor
 
-	bkgd(COLOR_PAIR(1));
+	// Get terminal size
 	int max_y, max_x;
 	getmaxyx(stdscr, max_y, max_x);
+
+	// Set up server name/active users
+	WINDOW *server_info_win = newwin(2, CHANNELS_PANE_WIDTH, 0, 0);
+	wcolor_set(server_info_win, COLPAIR_SERVER_INFO, NULL);
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < CHANNELS_PANE_WIDTH; j++) {
+			waddch(server_info_win, ACS_CKBOARD);
+		}
+		waddch(server_info_win, '\n');
+	}
+	mvwaddstr(server_info_win, 0, (CHANNELS_PANE_WIDTH - 11) / 2,
+			"Test Server");
+
+	// Set up message area
+	WINDOW *msg_win = newwin(max_y - 4, MSG_AREA_WIDTH, 2,
+			CHANNELS_PANE_WIDTH);
+	for (int i = 0; i < max_y - 4; i++) {
+		wmove(msg_win, i, 0);
+		for (int j = 0; j < MSG_AREA_WIDTH; j++) {
+			waddch(msg_win, '\'' | COLOR_PAIR(COLPAIR_MSG_BG));
+		}
+	}
+
+	// Draw the rest of the screen
 	mvprintw(max_y / 2, (max_x - 17) / 2, "%d cols X %d rows", max_x,
 			max_y);
-	box(stdscr, 0, 0);
-	mvadd_wch(0, 0, WACS_D_ULCORNER);
+	//box(stdscr, 0, 0);
+	/*border_set(WACS_D_VLINE, WACS_D_VLINE, WACS_D_HLINE, WACS_D_HLINE,
+			WACS_D_ULCORNER, WACS_D_URCORNER, WACS_D_LLCORNER,
+			WACS_D_LRCORNER);*/
+	//mvadd_wch(max_y - 1, 0, WACS_D_LLCORNER);
 
+	// Draw everything
 	refresh();
-	getch();
+	wrefresh(msg_win);
+	wrefresh(server_info_win);
+
+	getch(); // Wait for user input to terminate
 	endwin();
 
 	return 0;
